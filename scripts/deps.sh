06b448138f8ff9c4e57980c2c5d455c20bda2c2b
Scott Ivey <scott.ivey@gmail.com>
2016-09-25 19:25:40 -0400

    ok

---
+++ scripts/deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ROOT=$(git rev-parse --show-toplevel)
+
+EXT=${ROOT}/external
+
+
+function cmake-build() {
+    if [[ ! -d "build" ]]; then
+        mkdir build
+        pushd build
+        cmake ../ && make -j8
+        popd
+    fi
+}
+
+pushd ${EXT}/googletest
+cmake-build
+popd
+
+pushd ${EXT}/boringssl
+cmake-build
+popd
